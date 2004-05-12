3dda33d5704b04e8caa54a8553a05dd49cff8e8e
bradfitz <bradfitz@6caf28e9-730f-0410-b62b-a31386fe13fb>
2004-05-12 15:35:21 +0000

    add HTTP management support alongside the existing management port
    so it's either line-based interactive, or HTTP, if it detects an HTTP
    request.
    
    
    git-svn-id: http://code.sixapart.com/svn/perlbal/trunk@48 6caf28e9-730f-0410-b62b-a31386fe13fb

--- lib/Perlbal/ClientManage.pm
+++ lib/Perlbal/ClientManage.pm
@@ -5,7 +5,10 @@
 package Perlbal::ClientManage;
 use strict;
 use base "Perlbal::Socket";
-use fields qw(service buf);
+use fields ('service',
+            'buf',
+            'is_http',  # bool: is an HTTP request?
+            );  
 
 # ClientManage
 sub new {
@@ -20,11 +23,26 @@ sub new {
 
 # ClientManage
 sub event_read {
-    my $self = shift;
+    my Perlbal::ClientManage $self = shift;
 
-    my $bref = $self->read(1024);
-    return $self->close() unless defined $bref;
-    $self->{buf} .= $$bref;
+    my $bref;
+    unless ($self->{is_http}) {
+        $bref = $self->read(1024);
+        return $self->close() unless defined $bref;
+        $self->{buf} .= $$bref;
+
+        if ($self->{buf} =~ /^(?:HEAD|GET|POST) /) {
+            $self->{is_http} = 1;
+            $self->{headers_string} .= $$bref;
+        }
+    }
+
+    if ($self->{is_http}) {
+        my $hd = $self->read_request_headers;
+        return unless $hd;
+        $self->handle_http();
+        return;
+    }
 
     if ($self->{buf} =~ s/^(.+?)\r?\n//) {
         my $line = $1;
@@ -37,6 +55,41 @@ sub event_read {
 # ClientManage
 sub event_err {  my $self = shift; $self->close; }
 sub event_hup {  my $self = shift; $self->close; }
+
+# HTTP management support
+sub handle_http {
+    my Perlbal::ClientManage $self = shift;
+
+    my $uri = $self->{headers}->request_uri;
+
+    my $body;
+    my $code = "200 OK";
+
+    if ($uri eq "/") {
+        $body .= "<h1>perlbal management interface</h1>";
+        $body .= "<a href='/stats'>Server Stats</a>";
+    } elsif ($uri eq "/stats") {
+        my $sf = Perlbal::Socket->get_sock_ref;
+        $body .= "<table border='1' cellpadding='2'>";
+        $body .= "<tr><th>fd</th><th>age</th><th>description</th></tr>\n";
+        my $now = time;
+        foreach (sort { $a <=> $b } keys %$sf) {
+            my $sock = $sf->{$_};
+            my $age = $now - $sock->{create_time};
+            $body .= "<tr><td>$_</td><td>$age</td><td>" . $sock->as_string_html . "</td></tr>\n";
+        }
+        $body .= "</table>";
+    } else {
+        $code = "404 Not found";
+        $body .= "<h1>$code</h1>";
+    }
+
+    $body .= "<hr />Go to <a href='/'>top-level</a>.\n";
+    $self->write("HTTP/1.0 $code\r\nContent-type: text/html\r\nContent-Length: " . length($body) . 
+                 "\r\n\r\n$body");
+    $self->write(sub { $self->close; });
+    return;
+}
 
 1;
 
