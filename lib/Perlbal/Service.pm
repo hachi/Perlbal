760452fe0d1037235469e15b7b0ad7d33a243559
marksmith <marksmith@6caf28e9-730f-0410-b62b-a31386fe13fb>
2004-08-27 23:21:26 +0000

    Add in a hook that is called when a request that's not high priority is received.  The hook can be used to make a requet high priority if that is desired behavior.
    git-svn-id: http://code.sixapart.com/svn/perlbal/trunk@170 6caf28e9-730f-0410-b62b-a31386fe13fb

--- lib/Perlbal/Service.pm
+++ lib/Perlbal/Service.pm
@@ -404,6 +404,10 @@ sub request_backend_connection {
         $hi_pri = index($hicookie, $self->{high_priority_cookie_contents}) != -1;
         $cp->{high_priority} = 1 if $hi_pri;
     }
+    
+    # now, call hook to see if this should be high priority
+    $hi_pri = $self->run_hook('make_high_priority', $cp)
+        unless $hi_pri; # only if it's not already
 
     # before we even consider spawning backends, let's see if we have
     # some bored (pre-connected) backends that'd take this client
