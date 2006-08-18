99d01619e737f5c083ae869600d945375a9e357b
hachi <hachi@6caf28e9-730f-0410-b62b-a31386fe13fb>
2006-08-18 22:45:18 +0000

    r18104@lj (orig r524):  bradfitz | 2006-08-18 15:16:00 -0700  updating cache class
    git-svn-id: http://code.sixapart.com/svn/perlbal/trunk@541 6caf28e9-730f-0410-b62b-a31386fe13fb

--- lib/Perlbal/Cache.pm
+++ lib/Perlbal/Cache.pm
@@ -1,258 +1,235 @@
-#!/usr/bin/perl
-#
-# LJ::Cache class
-# See perldoc documentation at the end of this file.
-#
-# -------------------------------------------------------------------------
-#
-# This package is released under the LGPL (GNU Library General Public License)
-#
-# A copy of the license has been included with the software as LGPL.txt.  
-# If not, the license is available at:
-#      http://www.gnu.org/copyleft/library.txt
-#
-# -------------------------------------------------------------------------
-#
+# (This is a copy of Cache::SimpleLRU.)
+# License to use and redistribute this under the same terms as Perl itself.
 
 package Perlbal::Cache;
 
 use strict;
-no warnings; # This module is going to be replaced in a few hours by something shorter, make it quiet for now.
-use fields qw(items size tail head bytes maxsize maxbytes);
+use fields qw(items size tail head maxsize);
 
 use vars qw($VERSION);
-use constant PREVKEY => 0;
-use constant VALUE => 1;
-use constant NEXTKEY => 2;
-use constant BYTES => 3;
-use constant INSTIME => 4;
-use constant FLAGS => 5;     # caller-defined metadata
+use constant PREVREF => 0;  # ptr left, to newer item
+use constant VALUE   => 1;
+use constant NEXTREF => 2;  # ptr right, to older item
+use constant KEY     => 3;  # copy of key for unlinking from namespace on fallout
 
 $VERSION = '1.0';
 
 sub new {
-    my ($class, $args) = @_;
+    my $class = shift;
     my $self = fields::new($class);
-    
-    $self->init($args);
+    my $args = @_ == 1 ? $_[0] : { @_ };
+
+    $self->{head}    = undef,
+    $self->{tail}    = undef,
+    $self->{items}   = {}; # key -> arrayref, indexed by constants above
+    $self->{size}    = 0;
+    $self->{maxsize} = $args->{maxsize}+0;
     return $self;
 }
 
-sub walk_items {
+# need to DESTROY to cleanup doubly-linked list (circular refs)
+sub DESTROY {
+    my $self = shift;
+    $self->set_maxsize(0);
+    $self->validate_list;
+}
+
+# calls $code->($val) for each value in cache.  $code must return true
+# to continue walking.  foreach returns true if you hit the end.
+sub foreach {
     my Perlbal::Cache $self = shift;
     my $code = shift;
-
-    my $iter = $self->{'head'};
+    my $iter = $self->{head};
     while ($iter) {
-        my $it = $self->{'items'}->{$iter};
-        $code->($iter, $it->[BYTES], $it->[INSTIME]);
-        $iter = $it->[NEXTKEY];
+        my $val = $iter->[VALUE];
+        $iter = $iter->[NEXTREF];
+        last unless $code->($val);
     }
+    return $iter ? 0 : 1;
 }
 
-sub init {
+sub size {
     my Perlbal::Cache $self = shift;
-    my $args = shift;
-
-    $self->{'head'} = 0;
-    $self->{'tail'} = 0;
-    $self->{'items'} = {}; # key -> arrayref, indexed by constants above
-    $self->{'size'} = 0;
-    $self->{'bytes'} = 0;
-    $self->{'maxsize'} = $args->{'maxsize'}+0;
-    $self->{'maxbytes'} = $args->{'maxbytes'}+0;
+    return $self->{size};
 }
 
-sub get_item_count {
-    my Perlbal::Cache $self = shift;
-    $self->{'size'};
+sub set_maxsize {
+    my ($self, $maxsize) = @_;
+    $self->{maxsize} = $maxsize;
+    $self->drop_tail while
+        $self->{size} > $self->{maxsize};
 }
 
-sub get_byte_count {
-    my Perlbal::Cache $self = shift;
-    $self->{'bytes'};
-}
+# For debugging only
+sub validate_list {
+    my ($self) = @_;
 
-sub get_max_age {
-    my Perlbal::Cache $self = shift;
-    return undef unless $self->{'tail'};
-    return $self->{'items'}->{$self->{'tail'}}->[INSTIME];
-}
+    die "no tail pointer\n" if $self->{size} && ! $self->{tail};
+    die "no head pointer\n" if $self->{size} && ! $self->{head};
+    die "unwanted tail pointer\n" if ! $self->{size} && $self->{tail};
+    die "unwanted head pointer\n" if ! $self->{size} && $self->{head};
 
-sub validate_list
-{
-    my ($self, $source) = @_;
-    print "Validate list: $self->{'size'} (max: $self->{'maxsize'})\n";
-    
-    my $count = 1;
-    if ($self->{'size'} && ! defined $self->{'head'}) {
-	die "$source: no head pointer\n";
-    }
-    if ($self->{'size'} && ! defined $self->{'tail'}) {
-	die "$source: no tail pointer\n";
-    }
-    if ($self->{'size'}) {
-	print "  head: $self->{'head'}\n";
-	print "  tail: $self->{'tail'}\n";
-    }
-
-    my $iter = $self->{'head'};
+    my $iter = $self->{head};
     my $last = undef;
-    while ($count <= $self->{'size'}) {
-	if (! defined $iter) {
-	    die "$source: undefined iterator\n";
-	}
-	my $item = $self->{'items'}->{$iter};
-	unless (defined $item) {
-	    die "$source: item '$iter' isn't in items\n";
-	}
-	my $prevtext = $item->[PREVKEY] || "--";
-	my $nexttext = $item->[NEXTKEY] || "--";
-	print "  #$count ($iter): [$prevtext, $item->[VALUE], $nexttext]\n";
-	if ($count == 1 && defined($item->[0])) {
-	    die "$source: Head element shouldn't have previous pointer!\n";
-	}
-	if ($count == $self->{'size'} && defined($item->[NEXTKEY])) {
-	    die "$source: Last element shouldn't have next pointer!\n";
-	}
-	if (defined $last && ! defined $item->[PREVKEY]) {
-	    die "$source: defined \$last but not defined previous pointer.\n";
-	}
-	if (! defined $last && defined $item->[PREVKEY]) {
-	    die "$source: not defined \$last but previous pointer defined.\n";
-	}
-	if (defined $item->[PREVKEY] && defined $last && $item->[PREVKEY] ne $last)
-	{
-	    die "$source: Previous pointer is wrong.\n";
-	}
+    my $count = 1;
+    while ($count <= $self->{size}) {
+        if (! defined $iter) {
+            die "undefined iterator on element \#$count (trying to get to size $self->{size})\n";
+        }
+        my $key = $iter->[KEY];
+        my $it_via_hash = $self->{items}->{$key} or
+            die "item '$key' found in list, but not in hash\n";
 
-	$last = $iter;
-	$iter = defined $item->[NEXTKEY] ? $item->[NEXTKEY] : undef;
-	$count++;
+        unless ($it_via_hash == $iter) {
+            die "Hash value of '$key' maps to different node than we found.\n";
+        }
+
+        if ($count == 1 && $iter->[PREVREF]) {
+            die "Head element shouldn't have previous pointer!\n";
+        }
+        if ($count == $self->{size} && $iter->[NEXTREF]) {
+            die "Last element shouldn't have next pointer!\n";
+        }
+        if ($iter->[NEXTREF] && $iter->[NEXTREF]->[PREVREF] != $iter) {
+            die "next's previous should be us.\n";
+        }
+        if ($last && $iter->[PREVREF] != $last) {
+            die "defined \$last but its previous isn't us.\n";
+        }
+        if ($last && $last->[NEXTREF] != $iter) {
+            die "defined \$last but our next isn't it\n";
+        }
+        if (!$last && $iter->[PREVREF]) {
+            die "uh, we have a nextref but shouldn't\n";
+        }
+
+        $last = $iter;
+        $iter = $iter->[NEXTREF];
+        $count++;
     }
+    return 1;
 }
 
-sub maxsize
-{
+sub drop_tail {
     my Perlbal::Cache $self = shift;
-    $self->{maxsize} = shift;
-}
-
-sub drop_tail
-{
-    my Perlbal::Cache $self = shift;
+    die "no tail (size)" unless $self->{size};
 
     ## who's going to die?
-    my $to_die = $self->{'tail'};
+    my $to_die = $self->{tail} or die "no tail (key)";
 
     ## set the tail to the item before the one dying.
-    $self->{'tail'} = $self->{'items'}->{$to_die}->[PREVKEY];
+    $self->{tail} = $self->{tail}->[PREVREF];
 
     ## adjust the forward pointer on the tail to be undef
-    if (defined $self->{'tail'}) {
-	undef $self->{'items'}->{$self->{'tail'}}->[NEXTKEY];
+    if (defined $self->{tail}) {
+        $self->{tail}->[NEXTREF] = undef;
     }
 
     ## kill the item
-    my $bytes = $self->{'items'}->{$to_die}->[BYTES];
-    delete $self->{'items'}->{$to_die};
+    delete $self->{items}->{$to_die->[KEY]};
 
     ## shrink the overall size
-    $self->{'size'}--;
-    $self->{'bytes'} -= $bytes;
-}
+    $self->{size}--;
 
-sub print_list {
-    my Perlbal::Cache $self = shift;
-
-    print "Size: $self->{'size'} (max: $self->{'maxsize'})\n";
-
-    my $count = 1;
-    my $iter = $self->{'head'};
-    while (defined $iter) { #$count <= $self->{'size'}) {
-	my $item = $self->{'items'}->{$iter};
-	print "$count: $iter = $item->[VALUE]\n";
-	$iter = $item->[NEXTKEY];
- 	$count++;
+    if (!$self->{size}) {
+        $self->{head} = undef;
     }
 }
 
 sub get {
     my Perlbal::Cache $self = shift;
-    my ($key, $out_flags) = @_;
+    my ($key) = @_;
 
-    if (exists $self->{'items'}->{$key}) 
-    {
-	my $item = $self->{'items'}->{$key};
+    my $item = $self->{items}{$key} or
+        return undef;
 
-	# promote this to the head
-	unless ($self->{'head'} eq $key)
-	{
-	    if ($self->{'tail'} eq $key) {
-		$self->{'tail'} = $item->[PREVKEY];
-	    }
-	    # remove this element from the linked list.
-	    my $next = $item->[NEXTKEY];
-	    my $prev = $item->[PREVKEY];
-	    if (defined $next) { $self->{'items'}->{$next}->[PREVKEY] = $prev; }
-	    if (defined $prev) { $self->{'items'}->{$prev}->[NEXTKEY] = $next; }
-	    
-	    # make current head point backwards to this item
-	    $self->{'items'}->{$self->{'head'}}->[PREVKEY] = $key;
-	    
-	    # make this item point forwards to current head, and backwards nowhere
-	    $item->[NEXTKEY] = $self->{'head'};
-	    undef $item->[PREVKEY];
-	    
-	    # make this the new head
-	    $self->{'head'} = $key;
-	}
-	
-        $$out_flags = $item->[FLAGS] if $out_flags;
-	return $item->[VALUE];
+    # promote this to the head
+    unless ($self->{head} == $item) {
+        if ($self->{tail} == $item) {
+            $self->{tail} = $item->[PREVREF];
+        }
+
+        # remove this element from the linked list.
+        my $next = $item->[NEXTREF];
+        my $prev = $item->[PREVREF];
+        if ($next) { $next->[PREVREF] = $prev; }
+        if ($prev) { $prev->[NEXTREF] = $next; }
+
+        # make current head point backwards to this item
+        $self->{head}->[PREVREF] = $item;
+
+        # make this item point forwards to current head, and backwards nowhere
+        $item->[NEXTREF] = $self->{head};
+        $item->[PREVREF] = undef;
+
+        # make this the new head
+        $self->{head} = $item;
     }
-    return undef;
+
+    return $item->[VALUE];
 }
 
-# bytes is optional
+sub remove {
+    my Perlbal::Cache $self = shift;
+    my ($key) = @_;
+
+    my $item = $self->{items}{$key} or
+        return 0;
+    delete $self->{items}{$key};
+    $self->{size}--;
+
+    if (!$self->{size}) {
+        $self->{head} = undef;
+        $self->{tail} = undef;
+        return 1;
+    }
+
+    if ($self->{head} == $item) {
+        $self->{head} = $item->[NEXTREF];
+        $self->{head}->[PREVREF] = undef;
+        return 1;
+    }
+    if ($self->{tail} == $item) {
+        $self->{tail} = $item->[PREVREF];
+        $self->{tail}->[NEXTREF] = undef;
+        return 1;
+    }
+
+    # remove from middle
+    $item->[PREVREF]->[NEXTREF] = $item->[NEXTREF];
+    $item->[NEXTREF]->[PREVREF] = $item->[PREVREF];
+    return 1;
+
+}
+
 sub set {
     my Perlbal::Cache $self = shift;
-    my ($key, $value, $bytes, $flags) = @_;
-    
-    $self->drop_tail() while ($self->{'maxsize'} && 
-                              $self->{'size'} >= $self->{'maxsize'} &&
-                              ! exists $self->{'items'}->{$key}) ||
-                              ($self->{'maxbytes'} && $self->{'size'} &&
-                               $self->{'bytes'} + $bytes >= $self->{'maxbytes'} &&
-                               ! exists $self->{'items'}->{$key});
-    
-    
-    if (exists $self->{'items'}->{$key}) {
-	# update the value
-	my $it = $self->{'items'}->{$key};
-	$it->[VALUE] = $value;
-        my $bytedelta = $bytes - $it->[BYTES];
-        $self->{'bytes'} += $bytedelta;
-        $it->[BYTES] = $bytes;
-        $it->[FLAGS] = $flags;
-    } else {
-	# stick it at the end, for now
-	my $it = $self->{'items'}->{$key} = [];
-        $it->[PREVKEY] = undef;
-        $it->[NEXTKEY] = undef;
+    my ($key, $value) = @_;
+
+    $self->drop_tail while
+        $self->{maxsize} &&
+        $self->{size} >= $self->{maxsize} &&
+        ! exists $self->{items}->{$key};
+
+    if (exists $self->{items}->{$key}) {
+        # update the value
+        my $it = $self->{items}->{$key};
         $it->[VALUE] = $value;
-        $it->[BYTES] = $bytes;
-        $it->[INSTIME] = time();
-        $it->[FLAGS] = $flags;
-	if ($self->{'size'}) {
-	    $self->{'items'}->{$self->{'tail'}}->[NEXTKEY] = $key;
-	    $self->{'items'}->{$key}->[PREVKEY] = $self->{'tail'};
-	} else {
-	    $self->{'head'} = $key;
-	}
-	$self->{'tail'} = $key;
-	$self->{'size'}++;
-	$self->{'bytes'} += $bytes;
+    } else {
+        # stick it at the end, for now
+        my $it = $self->{items}->{$key} = [];
+        $it->[PREVREF] = undef;
+        $it->[NEXTREF] = undef;
+        $it->[KEY]     = $key;
+        $it->[VALUE] = $value;
+        if ($self->{size}) {
+            $self->{tail}->[NEXTREF] = $it;
+            $it->[PREVREF] = $self->{tail};
+        } else {
+            $self->{head} = $it;
+        }
+        $self->{tail} = $it;
+        $self->{size}++;
     }
 
     # this will promote it to the top:
@@ -260,39 +237,3 @@ sub set {
 }
 
 1;
-__END__
-
-=head1 NAME
-
-LJ::Cache - LRU Cache
-
-=head1 SYNOPSIS
-
-  use LJ::Cache;
-  my $cache = new LJ::Cache { 'maxsize' => 20 };
-  my $value = $cache->get($key);
-  unless (defined $value) {
-      $val = "load some value";
-      $cache->set($key, $value);
-  }
-
-=head1 DESCRIPTION
-
-This class implements an LRU dictionary cache.  The two operations on it
-are get() and set(), both of which promote the key being referenced to
-the "top" of the cache, so it will stay alive longest.
-
-When the cache is full and and a new item needs to be added, the oldest
-one is thrown away.
-
-You should be able to regenerate the data at any time, if get() 
-returns undef.
-
-This class is useful for caching information from a slower data source
-while also keeping a bound on memory usage.
-
-=head1 AUTHOR
-
-Brad Fitzpatrick, bradfitz@bradfitz.com
-
-=cut
