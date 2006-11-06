478fd6e93131f11a236571462e29495157ab4395
bradfitz <bradfitz@6caf28e9-730f-0410-b62b-a31386fe13fb>
2006-11-06 06:00:47 +0000

    +    -- AIO channels.  from the code: + +       "prevents all AIO threads from being consumed by requests for same +        failing/overloaded disk by isolating them into separate 'channels' in +        parent process and not dispatching more than the max in-flight count +        allows.  think of a channel as a named queue.  or in reality, a disk." +
    git-svn-id: http://code.sixapart.com/svn/perlbal/trunk@598 6caf28e9-730f-0410-b62b-a31386fe13fb

--- lib/Perlbal/AIO.pm
+++ lib/Perlbal/AIO.pm
@@ -5,6 +5,7 @@
 
 package Perlbal::AIO;
 
+use strict;
 use POSIX qw(ENOENT EACCES EBADF);
 use Fcntl qw(SEEK_CUR SEEK_SET SEEK_END O_RDWR O_CREAT O_TRUNC);
 
@@ -50,29 +51,195 @@ $Perlbal::AIO_MODE = "none";
 $Perlbal::AIO_MODE = "ioaio" if $Perlbal::OPTMOD_IO_AIO;
 $Perlbal::AIO_MODE = "linux" if $Perlbal::OPTMOD_LINUX_AIO;
 
+############################################################################
+# AIO functions available to callers
+############################################################################
 
 sub aio_readahead {
-    my ($fh, $offset, $length, $cb) = @_;
-    if ($Perlbal::AIO_MODE eq "linux") {
-        Linux::AIO::aio_readahead($fh, $offset, $length, $cb);
-    } elsif ($Perlbal::AIO_MODE eq "ioaio") {
-        IO::AIO::aio_readahead($fh, $offset, $length, $cb);
-    } else {
-        $cb->();
-    }
+    my ($fh, $offset, $length, $user_cb) = @_;
+
+    aio_channel_push(get_chan(), $user_cb, sub {
+        my $cb = shift;
+        if ($Perlbal::AIO_MODE eq "linux") {
+            Linux::AIO::aio_readahead($fh, $offset, $length, $cb);
+        } elsif ($Perlbal::AIO_MODE eq "ioaio") {
+            IO::AIO::aio_readahead($fh, $offset, $length, $cb);
+        } else {
+            $cb->();
+        }
+    });
 }
 
 sub aio_stat {
-    my ($file, $cb) = @_;
-    if ($Perlbal::AIO_MODE eq "linux") {
-        Linux::AIO::aio_stat($file, $cb);
-    } elsif ($Perlbal::AIO_MODE eq "ioaio") {
-        IO::AIO::aio_stat($file, $cb);
-    } else {
-        stat($file);
-        $cb->();
+    my ($file, $user_cb) = @_;
+
+    aio_channel_push(get_chan($file), $user_cb, sub {
+        my $cb = shift;
+        if ($Perlbal::AIO_MODE eq "linux") {
+            Linux::AIO::aio_stat($file, $cb);
+        } elsif ($Perlbal::AIO_MODE eq "ioaio") {
+            IO::AIO::aio_stat($file, $cb);
+        } else {
+            stat($file);
+            $cb->();
+        }
+    });
+}
+
+sub aio_open {
+    my ($file, $flags, $mode, $user_cb) = @_;
+
+    aio_channel_push(get_chan($file), $user_cb, sub {
+        my $cb = shift;
+
+        if ($Perlbal::AIO_MODE eq "linux") {
+            Linux::AIO::aio_open($file, $flags, $mode, sub {
+                my $fd = shift;
+                my $fh = _fh_of_fd_mode($fd, $mode);
+                $cb->($fh);
+            });
+        } elsif ($Perlbal::AIO_MODE eq "ioaio") {
+            IO::AIO::aio_open($file, $flags, $mode, $cb);
+        } else {
+            my $fh;
+            my $rv = sysopen($fh, $file, $flags, $mode);
+            $cb->($rv ? $fh : undef);
+        }
+    });
+}
+
+sub aio_unlink {
+    my ($file, $user_cb) = @_;
+    aio_channel_push(get_chan($file), $user_cb, sub {
+        my $cb = shift;
+
+        if ($Perlbal::AIO_MODE eq "linux") {
+            Linux::AIO::aio_unlink($file, $cb);
+        } elsif ($Perlbal::AIO_MODE eq "ioaio") {
+            IO::AIO::aio_unlink($file, $cb);
+        } else {
+            my $rv = unlink($file);
+            $rv = $rv ? 0 : -1;
+            $cb->($rv);
+        }
+    });
+}
+
+sub aio_write {
+    #   0    1        2        3(data) 4
+    my ($fh, $offset, $length, undef,  $user_cb) = @_;
+    return no_fh($user_cb) unless $fh;
+    my $alist = \@_;
+
+    aio_channel_push(get_chan(), $user_cb, sub {
+        my $cb = shift;
+        if ($Perlbal::AIO_MODE eq "linux") {
+            Linux::AIO::aio_write($fh, $offset, $length, $alist->[3], 0, $cb);
+        } elsif ($Perlbal::AIO_MODE eq "ioaio") {
+            IO::AIO::aio_write($fh, $offset, $length, $alist->[3], 0, $cb);
+        } else {
+            my $old_off = sysseek($fh, 0, SEEK_CUR);
+            sysseek($fh, $offset, 0);
+            my $rv = syswrite($fh, $alist->[3], $length, 0);
+            sysseek($fh, $old_off, SEEK_SET);
+            $cb->($rv);
+        }
+    });
+}
+
+sub aio_read {
+    #   0    1        2        3(data) 4
+    my ($fh, $offset, $length, undef,  $user_cb) = @_;
+    return no_fh($user_cb) unless $fh;
+    my $alist = \@_;
+
+    aio_channel_push(get_chan(), $user_cb, sub {
+        my $cb = shift;
+        if ($Perlbal::AIO_MODE eq "linux") {
+            Linux::AIO::aio_read($fh, $offset, $length, $alist->[3], 0, $cb);
+        } elsif ($Perlbal::AIO_MODE eq "ioaio") {
+            IO::AIO::aio_read($fh, $offset, $length, $alist->[3], 0, $cb);
+        } else {
+            my $old_off = sysseek($fh, 0, SEEK_CUR);
+            sysseek($fh, $offset, 0);
+            my $rv = sysread($fh, $alist->[3], $length, 0);
+            sysseek($fh, $old_off, SEEK_SET);
+            $cb->($rv);
+        }
+    });
+}
+
+############################################################################
+# AIO channel stuff
+#    prevents all AIO threads from being consumed by requests for same
+#    failing/overloaded disk by isolating them into separate 'channels' in
+#    parent process and not dispatching more than the max in-flight count
+#    allows.  think of a channel as a named queue.  or in reality, a disk.
+############################################################################
+
+my %chan_outstanding;  # $channel_name -> $num_in_flight
+my %chan_pending;      # $channel_name -> [ [$subref, $cb], .... ]
+my $use_aio_chans = 0; # keep them off for now, until mogstored code is ready to use them
+
+sub aio_channel_push {
+    my ($chan, $user_cb, $action) = @_;
+
+    # if we were to do it immediately, bypassing AIO channels (future option?)
+    unless ($use_aio_chans) {
+        $action->($user_cb);
+        return;
+    }
+
+    # IO::AIO/etc only take one callback.  so we wrap the user
+    # (caller) function with our own that first calls theirs, then
+    # does our bookkeeping and queue management afterwards.
+    my $wrapped_cb = sub {
+        $user_cb->(@_);
+        $chan_outstanding{$chan}--;
+        aio_channel_cond_run($chan);
+    };
+
+    # in case this is the first time this queue has been used, init stuff:
+    my $chanlist = ($chan_pending{$chan} ||= []);
+    $chan_outstanding{$chan} ||= 0;
+
+    # push a record representing the action to take later, then kick off queues
+    push @$chanlist, [$action, $wrapped_cb];
+    aio_channel_cond_run($chan);
+}
+
+sub aio_chan_max_concurrent {
+    my ($chan) = @_;
+    return 100 if $chan eq '[default]';
+    return 10;
+}
+
+sub aio_channel_cond_run {
+    my ($chan) = @_;
+
+    my $chanlist = $chan_pending{$chan} or return;
+    my $max_out  = aio_chan_max_concurrent($chan);
+
+    my $job;
+    while ($chan_outstanding{$chan} < $max_out && ($job = shift @$chanlist)) {
+        $chan_outstanding{$chan}++;
+        $job->[0]->($job->[1]);
     }
 }
+
+# gets currently-set channel, then clears it.  or if none set,
+# lets registered hook set the channel name from the optional
+# $file parameter.  the default channel, '[default]' has no limits
+sub get_chan {
+    return undef unless $use_aio_chans;
+    my ($file) = @_;
+    # TODO: pass to hook
+    return "[default]";
+}
+
+############################################################################
+# misc util functions
+############################################################################
 
 sub _fh_of_fd_mode {
     my ($fd, $mode) = @_;
@@ -82,72 +249,6 @@ sub _fh_of_fd_mode {
     my $fh = IO::Handle->new_from_fd($fd, 'r+');
     my $num = fileno($fh);
     return $fh;
-}
-
-sub aio_open {
-    my ($file, $flags, $mode, $cb) = @_;
-    if ($Perlbal::AIO_MODE eq "linux") {
-        Linux::AIO::aio_open($file, $flags, $mode, sub {
-            my $fd = shift;
-            my $fh = _fh_of_fd_mode($fd, $mode);
-            $cb->($fh);
-        });
-    } elsif ($Perlbal::AIO_MODE eq "ioaio") {
-        IO::AIO::aio_open($file, $flags, $mode, $cb);
-    } else {
-        my $fh;
-        my $rv = sysopen($fh, $file, $flags, $mode);
-        $cb->($rv ? $fh : undef);
-    }
-}
-
-sub aio_unlink {
-    my ($file, $cb) = @_;
-    if ($Perlbal::AIO_MODE eq "linux") {
-        Linux::AIO::aio_unlink($file, $cb);
-    } elsif ($Perlbal::AIO_MODE eq "ioaio") {
-        IO::AIO::aio_unlink($file, $cb);
-    } else {
-        my $rv = unlink($file);
-        $rv = $rv ? 0 : -1;
-        $cb->($rv);
-    }
-}
-
-sub aio_write {
-    #   0    1        2        3(data) 4
-    my ($fh, $offset, $length, undef,  $cb) = @_;
-    return no_fh($cb) unless $fh;
-
-    if ($Perlbal::AIO_MODE eq "linux") {
-        Linux::AIO::aio_write($fh, $offset, $length, $_[3], 0, $cb);
-    } elsif ($Perlbal::AIO_MODE eq "ioaio") {
-        IO::AIO::aio_write($fh, $offset, $length, $_[3], 0, $cb);
-    } else {
-        my $old_off = sysseek($fh, 0, SEEK_CUR);
-        sysseek($fh, $offset, 0);
-        my $rv = syswrite($fh, $_[3], $length, 0);
-        sysseek($fh, $old_off, SEEK_SET);
-        $cb->($rv);
-    }
-}
-
-sub aio_read {
-    #   0    1        2        3(data) 4
-    my ($fh, $offset, $length, undef,  $cb) = @_;
-    return no_fh($cb) unless $fh;
-
-    if ($Perlbal::AIO_MODE eq "linux") {
-        Linux::AIO::aio_read($fh, $offset, $length, $_[3], 0, $cb);
-    } elsif ($Perlbal::AIO_MODE eq "ioaio") {
-        IO::AIO::aio_read($fh, $offset, $length, $_[3], 0, $cb);
-    } else {
-        my $old_off = sysseek($fh, 0, SEEK_CUR);
-        sysseek($fh, $offset, 0);
-        my $rv = sysread($fh, $_[3], $length, 0);
-        sysseek($fh, $old_off, SEEK_SET);
-        $cb->($rv);
-    }
 }
 
 sub no_fh {
