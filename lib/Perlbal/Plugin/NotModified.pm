f9230433705c1d5be8dfde739e63a3a873c21043
hachi <hachi@6caf28e9-730f-0410-b62b-a31386fe13fb>
2007-03-20 02:19:37 +0000

    New plugin for serving 304 return codes when you have a url that won't ever change, but you don't want to hit a backend to check if it has changed.
    git-svn-id: http://code.sixapart.com/svn/perlbal/trunk@632 6caf28e9-730f-0410-b62b-a31386fe13fb

---
+++ lib/Perlbal/Plugin/NotModified.pm
@@ -0,0 +1,85 @@
+package Perlbal::Plugin::NotModified;
+
+use Perlbal;
+use strict;
+use warnings;
+
+# Takes settings in perlbal like:
+# SET ss.notmodified.host_pattern = ^example\.com
+#
+# The value is a regular expression to match against the Host: header on the incoming request.
+
+sub load {
+    my $class = shift;
+    return 1;
+}
+
+sub unload {
+    my $class = shift;
+    return 1;
+}
+
+# called when we're being added to a service
+sub register {
+    my ($class, $svc) = @_;
+
+    my $host_check_regex = undef;
+
+    my $start_http_request_hook =  sub {
+        my Perlbal::ClientHTTPBase $client = shift;
+        my Perlbal::HTTPHeaders $hds = $client->{req_headers};
+        return 0 unless $hds;
+
+        my $uri = $hds->request_uri;
+
+        return 0 unless $uri;
+        return 0 unless $uri =~ m!/!;
+
+        my $host = $hds->header("Host");
+
+        return 0 unless $host;
+        return 0 unless $host =~ $host_check_regex;
+
+        my $ims = $hds->header("If-Modified-Since");
+
+        return 0 unless $ims;
+        return 0 unless $ims =~ m!:!;
+
+        $client->send_response(304, "Not Modified.");
+        return 1;
+    };
+
+    # register things to take in configuration regular expressions
+    $svc->register_setter('NotModified', 'host_pattern', sub {
+        my ($out, $what, $val) = @_;
+        return 0 unless $what && $val;
+
+        my $err = sub {
+            $out->("ERROR: $_[0]") if $out;
+            return 0;
+        };
+
+        unless (length $val) {
+            $host_check_regex = undef;
+            $svc->unregister_hooks('NotModified');
+            return 1;
+        }
+
+        $host_check_regex = qr/$val/;
+        $svc->register_hook('NotModified', 'start_http_request', $start_http_request_hook);
+
+        return 1;
+    });
+
+    return 1;
+}
+
+# called when we're no longer active on a service
+sub unregister {
+    my ($class, $svc) = @_;
+    $svc->unregister_hooks('NotModified');
+    $svc->unregister_setters('NotModified');
+    return 1;
+}
+
+1;
