f58a1920f1f7716e8c23d26b47e81dca41fc4aec
hachi <hachi@6caf28e9-730f-0410-b62b-a31386fe13fb>
2010-02-22 14:27:35 +0000

    Setup keepalive state after merging reproxy results
    git-svn-id: http://code.sixapart.com/svn/perlbal/trunk@855 6caf28e9-730f-0410-b62b-a31386fe13fb

--- lib/Perlbal/BackendHTTP.pm
+++ lib/Perlbal/BackendHTTP.pm
@@ -504,10 +504,6 @@ sub handle_response { # : void
     my $res_source = $client->{primary_res_hdrs} || $hd;
     my $thd = $client->{res_headers} = $res_source->clone;
 
-    # setup_keepalive will set Connection: and Keep-Alive: headers for us
-    # as well as setup our HTTP version appropriately
-    $client->setup_keepalive($thd);
-
     # if we had an alternate primary response header, make sure
     # we send the real content-length (from the reproxied URL)
     # and not the one the first server gave us
@@ -527,6 +523,10 @@ sub handle_response { # : void
         } 
         $thd->code(200) if $thd->response_code == 204;  # upgrade HTTP No Content (204) to 200 OK.
     }
+
+    # setup_keepalive will set Connection: and Keep-Alive: headers for us
+    # as well as setup our HTTP version appropriately
+    $client->setup_keepalive($thd);
 
     print "  writing response headers to client\n" if Perlbal::DEBUG >= 3;
     $client->write($thd->to_string_ref);
